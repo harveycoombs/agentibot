34564de8cb44235929df969dc413fcef2773bce5
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-07-05 17:19:58 +0100

    Added loading messages for models

--- src/ai/models.py
+++ src/ai/models.py
@@ -1,24 +1,28 @@
 from langchain_community.llms import LlamaCpp
 
+print("Loading Qwen 3 30B A3B...")
+
 qwen_30b_a3b_iq3_m = LlamaCpp(
     model_path="../models/Qwen_Qwen3-30B-A3B-IQ3_M.gguf",
-    n_gpu_layers=-1,
-    n_batch=512,
-    temperature=0.8,
-    max_tokens=1500,
+    n_gpu_layers=24,
+    n_batch=128,
+    temperature=0.7,
+    max_tokens=1024,
     top_p=0.9,
-    n_ctx=32768,
+    n_ctx=8192,
     verbose=False
 )
 
+print("Loading Llama 4 Scout 17B 16E Instruct...")
+
 llama_4_scout_17b_16e_i1 = LlamaCpp(
     model_path="../models/Llama-4-Scout-17B-16E-Instruct-UD-IQ1_M.gguf",
-    n_gpu_layers=-1,
-    n_batch=512,
-    temperature=0.8,
-    max_tokens=1500,
+    n_gpu_layers=28,
+    n_batch=192,
+    temperature=0.7,
+    max_tokens=1200,
     top_p=0.9,
-    n_ctx=32768,
+    n_ctx=8192,
     verbose=False
 )
 
