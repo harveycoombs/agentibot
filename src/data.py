5b0ef1f037a7a4cd7fa9240ce7155c76990114fe
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-06 22:46:20 +0100

    Switched to use PostgreSQL instead of MySQL

--- src/data.py
+++ src/data.py
@@ -1,4 +1,4 @@
-import mysql.connector
+import psycopg
 from datetime import datetime
 import yaml
 
@@ -8,22 +8,22 @@ def register_guild(guild_id, owner_id):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
         cursor.execute("INSERT INTO registered_guilds (guild_id, owner_id, registration_date, interactions_this_month, interaction_start_date) VALUES (%s, %s, %s, %s, %s)", (guild_id, owner_id, datetime.now(), 0, datetime.now()))
         connection.commit()
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to register guild: {e}")
         return None
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
 
@@ -31,22 +31,22 @@ def update_registered_guild_owner(guild_id, owner_id):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
         cursor.execute("UPDATE registered_guilds SET owner_id = %s WHERE guild_id = %s", (owner_id, guild_id))
         connection.commit()
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to update registered guild owner: {e}")
         return None
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()    
 
@@ -54,14 +54,14 @@ def get_guild_interaction_count(guild_id):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
         cursor.execute("SELECT interactions_this_month FROM registered_guilds WHERE guild_id = %s", (guild_id,))
         result = cursor.fetchone()
@@ -70,11 +70,11 @@ def get_guild_interaction_count(guild_id):
             return 0
 
         return result["interactions_this_month"]
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to get guild interaction count: {e}")
         return 0
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
 
@@ -82,24 +82,24 @@ def update_guild_interaction_count(guild_id):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
         cursor.execute("UPDATE registered_guilds SET interactions_this_month = interactions_this_month + 1 WHERE guild_id = %s", (guild_id,))
         connection.commit()
 
         return cursor.rowcount > 0
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to update guild interaction count: {e}")
         return False
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
 
@@ -107,24 +107,24 @@ def check_guild_interaction_limit_hit(guild_id):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT COUNT(*) AS n FROM registered_guilds WHERE interactions_this_month >= 200 AND interaction_start_date >= DATE_SUB(NOW(), INTERVAL 1 MONTH) AND guild_id = %s", (guild_id,))
+        cursor.execute("SELECT COUNT(*) AS n FROM registered_guilds WHERE interactions_this_month >= 200 AND interaction_start_date >= NOW() - INTERVAL '1 month' AND guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         return result["n"] > 0
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to check guild interaction limit hit: {e}")
         return None
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
 
@@ -132,24 +132,24 @@ def guild_is_registered(guild_id):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
         cursor.execute("SELECT COUNT(*) AS n FROM registered_guilds WHERE guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         return result["n"] > 0
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to check if guild is registered: {e}")
         return False
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
 
@@ -157,24 +157,24 @@ def insert_error_log(guild_id, author_id, prompt, error_message):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("INSERT INTO error_logs (log_id, incident_date, guild_id, author_id, prompt, error) VALUES(UUID(), NOW(), %s, %s, %s, %s)", (guild_id, author_id, prompt, error_message))
+        cursor.execute("INSERT INTO errors (error_id, incident_date, guild_id, author_id, prompt, error) VALUES(gen_random_uuid(), NOW(), %s, %s, %s, %s)", (guild_id, author_id, prompt, error_message))
         connection.commit()
 
         return cursor.rowcount > 0
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to update guild interaction count: {e}")
         return False
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
 
@@ -182,23 +182,23 @@ def get_settings(guild_id):
     connection = None
 
     try:
-        connection = mysql.connector.connect(
+        connection = psycopg.connect(
             host=CONFIG["database"]["host"],
             user=CONFIG["database"]["user"],
             password=CONFIG["database"]["password"],
-            database=CONFIG["database"]["schema"]
+            dbname=CONFIG["database"]["schema"]
         )
 
-        cursor = connection.cursor(dictionary=True)
+        cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
         cursor.execute("SELECT * FROM user_settings WHERE user_id = (SELECT owner_id FROM registered_guilds WHERE guild_id = %s)", (guild_id,))
         result = cursor.fetchone()
 
         return result
-    except mysql.connector.Error as e:
+    except psycopg.Error as e:
         print(f"Unable to get user settings: {e}")
         return None
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
