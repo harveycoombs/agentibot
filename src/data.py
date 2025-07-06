87c845cd817e7b06a986b0824ed3cdb32aaabcdc
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-06 22:47:42 +0100

    schema specification fixes

--- src/data.py
+++ src/data.py
@@ -17,7 +17,7 @@ def register_guild(guild_id, owner_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("INSERT INTO registered_guilds (guild_id, owner_id, registration_date, interactions_this_month, interaction_start_date) VALUES (%s, %s, %s, %s, %s)", (guild_id, owner_id, datetime.now(), 0, datetime.now()))
+        cursor.execute("INSERT INTO vesper.registered_guilds (guild_id, owner_id, registration_date, interactions_this_month, interaction_start_date) VALUES (%s, %s, %s, %s, %s)", (guild_id, owner_id, datetime.now(), 0, datetime.now()))
         connection.commit()
     except psycopg.Error as e:
         print(f"Unable to register guild: {e}")
@@ -40,7 +40,7 @@ def update_registered_guild_owner(guild_id, owner_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("UPDATE registered_guilds SET owner_id = %s WHERE guild_id = %s", (owner_id, guild_id))
+        cursor.execute("UPDATE vesper.registered_guilds SET owner_id = %s WHERE guild_id = %s", (owner_id, guild_id))
         connection.commit()
     except psycopg.Error as e:
         print(f"Unable to update registered guild owner: {e}")
@@ -63,7 +63,7 @@ def get_guild_interaction_count(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT interactions_this_month FROM registered_guilds WHERE guild_id = %s", (guild_id,))
+        cursor.execute("SELECT interactions_this_month FROM vesper.registered_guilds WHERE guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         if result is None:
@@ -91,7 +91,7 @@ def update_guild_interaction_count(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("UPDATE registered_guilds SET interactions_this_month = interactions_this_month + 1 WHERE guild_id = %s", (guild_id,))
+        cursor.execute("UPDATE vesper.registered_guilds SET interactions_this_month = interactions_this_month + 1 WHERE guild_id = %s", (guild_id,))
         connection.commit()
 
         return cursor.rowcount > 0
@@ -116,7 +116,7 @@ def check_guild_interaction_limit_hit(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT COUNT(*) AS n FROM registered_guilds WHERE interactions_this_month >= 200 AND interaction_start_date >= NOW() - INTERVAL '1 month' AND guild_id = %s", (guild_id,))
+        cursor.execute("SELECT COUNT(*) AS n FROM vesper.registered_guilds WHERE interactions_this_month >= 200 AND interaction_start_date >= NOW() - INTERVAL '1 month' AND guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         return result["n"] > 0
@@ -141,7 +141,7 @@ def guild_is_registered(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT COUNT(*) AS n FROM registered_guilds WHERE guild_id = %s", (guild_id,))
+        cursor.execute("SELECT COUNT(*) AS n FROM vesper.registered_guilds WHERE guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         return result["n"] > 0
@@ -166,7 +166,7 @@ def insert_error_log(guild_id, author_id, prompt, error_message):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("INSERT INTO errors (error_id, incident_date, guild_id, author_id, prompt, error) VALUES(gen_random_uuid(), NOW(), %s, %s, %s, %s)", (guild_id, author_id, prompt, error_message))
+        cursor.execute("INSERT INTO vesper.errors (error_id, incident_date, guild_id, author_id, prompt, error) VALUES(gen_random_uuid(), NOW(), %s, %s, %s, %s)", (guild_id, author_id, prompt, error_message))
         connection.commit()
 
         return cursor.rowcount > 0
@@ -191,7 +191,7 @@ def get_settings(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT * FROM user_settings WHERE user_id = (SELECT owner_id FROM registered_guilds WHERE guild_id = %s)", (guild_id,))
+        cursor.execute("SELECT * FROM vesper.user_settings WHERE user_id = (SELECT owner_id FROM vesper.registered_guilds WHERE guild_id = %s)", (guild_id,))
         result = cursor.fetchone()
 
         return result
