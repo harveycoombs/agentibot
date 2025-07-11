19c41dbd33e0d72c06a32c031517d23f9e1ab475
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-11 22:54:18 +0100

    context window definition

--- src/ai/model.py
+++ src/ai/model.py
@@ -5,6 +5,7 @@ model = VLLM(
     trust_remote_code=True,
     vllm_kwargs={"quantization": "q4_k_m"},
     max_new_tokens=2048,
+    max_model_len=2048,
     top_k=10,
     top_p=0.95,
     temperature=0.85,
