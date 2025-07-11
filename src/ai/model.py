d17bd16f198c55caff4ed5bf1b4865c96ddbe152
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-11 23:00:31 +0100

    bnb

--- src/ai/model.py
+++ src/ai/model.py
@@ -1,13 +1,15 @@
 from langchain_community.llms import VLLM
 
 model = VLLM(
-    model="unsloth/Qwen3-8B-GGUF",
+    model="unsloth/Qwen3-8B-bnb-4bit",
     trust_remote_code=True,
-    vllm_kwargs={"quantization": "q4_k_m"},
     max_new_tokens=2048,
-    max_model_len=2048,
     top_k=10,
     top_p=0.95,
     temperature=0.85,
-    tensor_parallel_size=1
+    tensor_parallel_size=1,
+    vllm_kwargs={
+    #    "quantization": "q4_k_m",
+        "max_model_len": 2048
+    }
 )
