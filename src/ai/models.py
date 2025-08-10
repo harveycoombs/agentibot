726f2da17c4cc62c0ba0d449b4793e11d10f9177
Harvey Coombs <harveycoombs23@outlook.com>
2025-08-10 23:07:07 +0100

    Temporarily disabled GPT OSS

--- src/ai/models.py
+++ src/ai/models.py
@@ -37,18 +37,18 @@ gpt = ChatOpenAI(
     top_k=10
 )
 
-gpt_oss = VLLM(
-    model="unsloth/gpt-oss-20b-GGUF",
-    trust_remote_code=True,
-    max_new_tokens=2048,
-    top_k=10,
-    top_p=0.95,
-    temperature=0.85,
-    tensor_parallel_size=1,
-    vllm_kwargs={
-        "max_model_len": 2048
-    }
-)
+#gpt_oss = VLLM(
+#    model="unsloth/gpt-oss-20b-GGUF",
+#    trust_remote_code=True,
+#    max_new_tokens=2048,
+#    top_k=10,
+#    top_p=0.95,
+#    temperature=0.85,
+#    tensor_parallel_size=1,
+#    vllm_kwargs={
+#        "max_model_len": 2048
+#    }
+#)
 
 models = {
     "qwen": qwen,
