67be2c52871fbc9203804f3374110505a7187a68
Harvey Coombs <harveycoombs23@outlook.com>
2025-08-24 01:37:46 +0100

    yeah

--- src/ai/models.py
+++ src/ai/models.py
@@ -32,9 +32,7 @@ gpt = ChatOpenAI(
     api_key=CONFIG["openai_api_key"],
     model="gpt-5-mini",
     temperature=0.85,
-    max_tokens=2048#,
-    #top_p=0.95,
-    #top_k=10
+    max_tokens=2048
 )
 
 #gpt_oss = VLLM(
