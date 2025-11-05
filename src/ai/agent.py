11821d807f3eba557ae382e2939d14a3ab84f2be
harveycoombs <harveycoombs23@outlook.com>
2025-11-05 19:41:34 +0000

    GPT-5 full implementation

--- src/ai/agent.py
+++ src/ai/agent.py
@@ -91,8 +91,8 @@ class Agent:
 
         self.agent = initialize_agent(
             tools=self.tools,
-            llm=models[self.model if len(self.model) > 0 else "mistral"],
-            agent=AgentType.OPENAI_MULTI_FUNCTIONS if self.model == "gpt" else AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            llm=models[self.model if len(self.model) > 0 else "gpt-5-nano"],
+            agent=AgentType.OPENAI_MULTI_FUNCTIONS if self.model.startswith("gpt") else AgentType.ZERO_SHOT_REACT_DESCRIPTION,
             verbose=False,
             handle_parsing_errors=True
         )
--- src/ai/models.py
+++ src/ai/models.py
@@ -5,23 +5,29 @@ from langchain_anthropic import ChatAnthropic
 
 CONFIG = yaml.safe_load(open(f"{os.getcwd().replace("\\", "/")}/config.yaml"))
 
-gpt = ChatOpenAI(
+gpt5_nano = ChatOpenAI(
+    api_key=CONFIG["openai_api_key"],
+    model="gpt-5-nano",
+    temperature=0.85,
+    max_tokens=2048
+)
+
+gpt5_mini = ChatOpenAI(
     api_key=CONFIG["openai_api_key"],
     model="gpt-5-mini",
     temperature=0.85,
     max_tokens=2048
 )
 
-claude = ChatAnthropic(
-    api_key=CONFIG["anthropic_api_key"],
-    model="claude-3-5-haiku-20241022",
+gpt5 = ChatOpenAI(
+    api_key=CONFIG["openai_api_key"],
+    model="gpt-5",
     temperature=0.85,
-    max_tokens=2048,
-    top_p=0.95,
-    top_k=10
+    max_tokens=2048
 )
 
 models = {
-    "gpt": gpt,
-    "claude": claude
+    "gpt-5-nano": gpt5_nano,
+    "gpt-5-mini": gpt5_mini,
+    "gpt-5": gpt5
 }
