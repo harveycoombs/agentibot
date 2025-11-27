17ed1e0c6d3ad7f56644db7309a00abe69cad558
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-11-27 13:19:53 +0000

    Prevention of crashes

--- src/vesper.py
+++ src/vesper.py
@@ -1,6 +1,14 @@
 # Vesper vesperbot.ai ~ Written by Harvey Coombs ~ 2020-2024
 import asyncio
-
 from discord.gateway import Gateway
 
-asyncio.run(Gateway.connect())
+async def main():
+    while True:
+        try:
+            await Gateway.connect()
+        except Exception as ex:
+            print(f"Discord gateway raised an exception: {ex.message}")
+
+        await asyncio.sleep(4)
+
+asyncio.run(main())
