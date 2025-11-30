d7aeb7c6dbbbfce2c9186d91b32c94c2633f1ed4
harveycoombs <harveycoombs23@outlook.com>
2025-11-30 13:33:19 +0000

    Updated table name

--- src/data.py
+++ src/data.py
@@ -29,7 +29,7 @@ def register_guild(guild_id, owner_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("INSERT INTO vesper.registered_guilds (guild_id, owner_id, registration_date, interactions_this_month, interaction_start_date) VALUES (%s, %s, %s, %s, %s)", (guild_id, owner_id, datetime.now(), 0, datetime.now()))
+        cursor.execute("INSERT INTO vesper.guilds (guild_id, owner_id, registration_date, interactions_this_month, interaction_start_date) VALUES (%s, %s, %s, %s, %s)", (guild_id, owner_id, datetime.now(), 0, datetime.now()))
         connection.commit()
     except psycopg.Error as e:
         print(f"Unable to register guild: {e}")
@@ -52,7 +52,7 @@ def update_registered_guild_owner(guild_id, owner_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("UPDATE vesper.registered_guilds SET owner_id = %s WHERE guild_id = %s", (owner_id, guild_id))
+        cursor.execute("UPDATE vesper.guilds SET owner_id = %s WHERE guild_id = %s", (owner_id, guild_id))
         connection.commit()
     except psycopg.Error as e:
         print(f"Unable to update registered guild owner: {e}")
@@ -75,7 +75,7 @@ def get_guild_interaction_count(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT interactions_this_month FROM vesper.registered_guilds WHERE guild_id = %s", (guild_id,))
+        cursor.execute("SELECT interactions_this_month FROM vesper.guilds WHERE guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         if result is None:
@@ -103,7 +103,7 @@ def update_guild_interaction_count(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("UPDATE vesper.registered_guilds SET interactions_this_month = interactions_this_month + 1 WHERE guild_id = %s", (guild_id,))
+        cursor.execute("UPDATE vesper.guilds SET interactions_this_month = interactions_this_month + 1 WHERE guild_id = %s", (guild_id,))
         connection.commit()
 
         return cursor.rowcount > 0
@@ -128,7 +128,7 @@ def check_guild_interaction_limit_hit(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT COUNT(*) AS n FROM vesper.registered_guilds WHERE interactions_this_month >= 200 AND interaction_start_date >= NOW() - INTERVAL '1 month' AND guild_id = %s", (guild_id,))
+        cursor.execute("SELECT COUNT(*) AS n FROM vesper.guilds WHERE interactions_this_month >= 200 AND interaction_start_date >= NOW() - INTERVAL '1 month' AND guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         return result["n"] > 0
@@ -153,7 +153,7 @@ def guild_is_registered(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT COUNT(*) AS n FROM vesper.registered_guilds WHERE guild_id = %s", (guild_id,))
+        cursor.execute("SELECT COUNT(*) AS n FROM vesper.guilds WHERE guild_id = %s", (guild_id,))
         result = cursor.fetchone()
 
         return result["n"] > 0
@@ -203,7 +203,7 @@ def get_model_choice(guild_id):
 
         cursor = connection.cursor(row_factory=psycopg.rows.dict_row)
 
-        cursor.execute("SELECT model FROM vesper.users WHERE user_id = (SELECT owner_id FROM vesper.registered_guilds WHERE guild_id = %s)", (guild_id,))
+        cursor.execute("SELECT model FROM vesper.users WHERE user_id = (SELECT owner_id FROM vesper.guilds WHERE guild_id = %s)", (guild_id,))
         result = cursor.fetchone()
 
         if result is None:
