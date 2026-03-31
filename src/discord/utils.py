0a32c116346bc28d90b76aeca45a38a1f2ae278b
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2026-03-31 14:19:51 +0100

    test

--- src/discord/utils.py
+++ src/discord/utils.py
@@ -5,4 +5,5 @@ class Utils:
     def snowflake_to_datetime(snowflake: int):
         timestamp = (snowflake >> 22) + 1420070400000
         date = datetime.fromtimestamp(timestamp / 1000)
+
         return date.strftime("%d %B %Y")
