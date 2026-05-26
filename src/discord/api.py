73cacdbc05171ecbd0125496c7a517801609268e
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2026-05-27 00:10:31 +0100

    Improved API exception handling

--- src/discord/api.py
+++ src/discord/api.py
@@ -18,15 +18,20 @@ class DiscordAPI:
 
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{DiscordAPI.BASE_URL}{endpoint}", headers=headers) as response:
-                if response.status == 429:
-                    raise VesperException(":warning: You are being rate limited. Please try again later.")
-                elif response.status >= 400:
-                    raise VesperException(await response.text())
-
-                try:
-                    return await response.json()
-                except:
-                    return await response.text()
+                match response.status:
+                    case 429:
+                        raise VesperException(":warning: You are being rate limited. Please try again later.")
+                    case 403:
+                        raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                    case 400 | 404:
+                        raise VesperException(await response.text())
+                    case 500 | 503:
+                        raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                    case 200 | 201:
+                        try:
+                            return await response.json()
+                        except:
+                            return await response.text()
 
     @staticmethod
     async def post(endpoint, payload, additional_headers=None):
@@ -41,20 +46,36 @@ class DiscordAPI:
         async with aiohttp.ClientSession() as session:
             if payload is not None: 
                 async with session.post(f"{DiscordAPI.BASE_URL}{endpoint}", json=payload, headers=headers) as response:
-                    if response.status >= 400:
-                        raise VesperException(await response.text())
-
-                    try:
-                        return await response.json()
-
-                    except:
-                        return await response.text()
+                    match response.status:
+                        case 429:
+                            raise VesperException(":warning: You are being rate limited. Please try again later.")
+                        case 403:
+                            raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                        case 400 | 404:
+                            raise VesperException(await response.text())
+                        case 500 | 503:
+                            raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                        case 200 | 201:
+                            try:
+                                return await response.json()
+                            except:
+                                return await response.text()
             else:
                 async with session.post(f"{DiscordAPI.BASE_URL}{endpoint}", headers=headers) as response:
-                    try:
-                        return await response.json()
-                    except:
-                        return await response.text()
+                    match response.status:
+                        case 429:
+                            raise VesperException(":warning: You are being rate limited. Please try again later.")
+                        case 403:
+                            raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                        case 400 | 404:
+                            raise VesperException(await response.text())
+                        case 500 | 503:
+                            raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                        case 200 | 201:
+                            try:
+                                return await response.json()
+                            except:
+                                return await response.text()
 
     @staticmethod
     async def patch(endpoint, payload, additional_headers=None):
@@ -68,13 +89,20 @@ class DiscordAPI:
 
         async with aiohttp.ClientSession() as session:
             async with session.patch(f"{DiscordAPI.BASE_URL}{endpoint}", json=payload, headers=headers) as response:
-                if response.status >= 400:
-                    raise VesperException(await response.text())
-
-                try:
-                    return await response.json()
-                except:
-                    return await response.text()
+                match response.status:
+                    case 429:
+                        raise VesperException(":warning: You are being rate limited. Please try again later.")
+                    case 403:
+                        raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                    case 400 | 404:
+                        raise VesperException(await response.text())
+                    case 500 | 503:
+                        raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                    case 200 | 201:
+                        try:
+                            return await response.json()
+                        except:
+                            return await response.text()
 
     @staticmethod
     async def put(endpoint, payload, additional_headers=None):
@@ -89,19 +117,36 @@ class DiscordAPI:
         async with aiohttp.ClientSession() as session:
             if payload is not None:
                 async with session.put(f"{DiscordAPI.BASE_URL}{endpoint}", json=payload, headers=headers) as response:
-                    if response.status >= 400:
-                        raise VesperException(await response.text())
-
-                    try:
-                        return await response.json()
-                    except:
-                        return await response.text()
+                    match response.status:
+                        case 429:
+                            raise VesperException(":warning: You are being rate limited. Please try again later.")
+                        case 403:
+                            raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                        case 400 | 404:
+                            raise VesperException(await response.text())
+                        case 500 | 503:
+                            raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                        case 200 | 201:
+                            try:
+                                return await response.json()
+                            except:
+                                return await response.text()
             else:
                 async with session.put(f"{DiscordAPI.BASE_URL}{endpoint}", headers=headers) as response:
-                    try:
-                        return await response.json()
-                    except:
-                        return await response.text()
+                    match response.status:
+                        case 429:
+                            raise VesperException(":warning: You are being rate limited. Please try again later.")
+                        case 403:
+                            raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                        case 400 | 404:
+                            raise VesperException(await response.text())
+                        case 500 | 503:
+                            raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                        case 200 | 201:
+                            try:
+                                return await response.json()
+                            except:
+                                return await response.text()
 
     @staticmethod
     async def delete(endpoint, payload=None, additional_headers=None):
@@ -116,19 +161,33 @@ class DiscordAPI:
         async with aiohttp.ClientSession() as session:
             if payload is not None:
                 async with session.delete(f"{DiscordAPI.BASE_URL}{endpoint}", json=payload, headers=headers) as response:
-                    if response.status >= 400:
-                        raise VesperException(await response.text())
-
-                    try:
-                        return await response.json()
-                    except:
-                        return await response.text()
+                    match response.status:
+                        case 429:
+                            raise VesperException(":warning: You are being rate limited. Please try again later.")
+                        case 403:
+                            raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                        case 400 | 404:
+                            raise VesperException(await response.text())
+                        case 500 | 503:
+                            raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                        case 200 | 201:
+                            try:
+                                return await response.json()
+                            except:
+                                return await response.text()
             else:
                 async with session.delete(f"{DiscordAPI.BASE_URL}{endpoint}", headers=headers) as response:
-                    if response.status >= 400:
-                        raise VesperException(await response.text())
-
-                    try:
-                        return await response.json()
-                    except:
-                        return await response.text()
+                    match response.status:
+                        case 429:
+                            raise VesperException(":warning: You are being rate limited. Please try again later.")
+                        case 403:
+                            raise VesperException(":no_entry_sign: I do not have permission to perform this action.")
+                        case 400 | 404:
+                            raise VesperException(await response.text())
+                        case 500 | 503:
+                            raise VesperException(":bangbang: Something went wrong. If this issue persists, [Contact Support](https://www.vesperbot.ai/contact) for further assistance.")
+                        case 200 | 201:
+                            try:
+                                return await response.json()
+                            except:
+                                return await response.text()
